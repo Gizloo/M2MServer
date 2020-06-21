6e6df72420be9345b545f6866d472866710ea08f
Михаил Переводов <perevodovmv@yandex.ru>
2020-06-21 11:41:37 +0700

    Второй ответ для 1С

--- down_data.py
+++ down_data.py
@@ -1,10 +1,10 @@
 # coding: utf8
+from pprint import pprint
 
-from wialon import flags, Wialon
+from wialon import flags, Wialon, WialonError
 
 
 def api_wialon_dwnData(wialon):
-    res_id = None
     spec = {
         'itemsType': 'avl_resource',
         'propName': 'sys_name',
@@ -16,6 +16,33 @@ def api_wialon_dwnData(wialon):
     data = wialon.core_search_items(spec=spec, force=1, flags=custom_flag, **interval)
     res_id = None
     for count in data['items']:
-    	if 'api' in str(count['nm']):
+        if 'api' in str(count['nm']):
             res_id = count['id']
+
     return res_id
+
+
+def api_wialon_dwnObj(wialon):
+    spec = {
+        'itemsType': 'avl_unit',
+        'propName': 'sys_name',
+        'propValueMask': '*',
+        'sortType': 'sys_name'
+    }
+    interval = {"from": 0, "to": 0}
+    custom_flag = flags.ITEM_DATAFLAG_BASE
+    data = wialon.core_search_items(spec=spec, force=1, flags=custom_flag, **interval)
+    # pprint(data)
+    for key in data['items']:
+        print(f'{key["nm"]} : {key["id"]}')
+
+# token = 'db1cee3b1f964df20f8d163a1423b6c6286A919144720D152383E5DD77C6113AD31CDC9A'
+# wialon = Wialon()
+# login = None
+# try:
+#     login = wialon.token_login(token=str(token))
+# except WialonError as e:
+#     print('Error while login')
+# wialon.sid = login['eid']
+# res_id = api_wialon_dwnData(wialon)
+# print(res_id)
--- probe.py
+++ probe.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 from wialon import Wialon, WialonError, flags
 
-token = 'e5023bcf1b3d6fa822c0f3b8d03759d5C81B9FB7768294A92AD2138C5F516C26FF757672'
+token = '5978d12e7eb520190d8c97cbc4ac2fbc6708856600D7CB37E18093DD3C6B787DB7D551BA'
 wialon = Wialon()
 login = None
 try:
@@ -28,52 +28,58 @@ def api_wialon_dwnData(wialon):
 
 
 data = api_wialon_dwnData(wialon)
-pprint(data)
+# pprint(data)
 timeFrom = 1590944400
 timeTo = 1592326740
 
 unit1 = wialon.messages_load_interval({
 
-    "itemId": 21092790,
+    "itemId": 17323425,
     "timeFrom": timeFrom,
     "timeTo": timeTo,
     "flags": 0x0000,
     "flagsMask": 0xFF00,
     "loadCount": 0xffffffff})
 
-# pprint(unit1)
 mess_block = unit1['messages']
 first_mess_coord_x = None
 first_mess_coord_y = None
 
 for mess in mess_block:
-    if mess['pos']['x']:
-        first_mess_coord_x = mess['pos']['x']
-        first_mess_coord_y = mess['pos']['y']
-        break
+    if mess['pos'] is not None:
+        if mess['pos']['x']:
+            first_mess_coord_x = mess['pos']['x']
+            first_mess_coord_y = mess['pos']['y']
+            break
 
 prostrel = 0
+prostrel_dist = 0
 prostrel_time = []
 print(first_mess_coord_x)
 print(first_mess_coord_y)
 
+
 for mess in mess_block:
-    if mess['pos']['x']:
-        dist = ((((mess['pos']['x']) - first_mess_coord_x)**2+((mess['pos']['y']) - first_mess_coord_y)**2)**0.5)*70000
-        if dist > 400:
-            prostrel += 1
+    if mess['pos'] is not None:
+        if mess['pos']['x'] is not None:
+            dist = ((((mess['pos']['x']) - first_mess_coord_x)**2+((mess['pos']['y']) - first_mess_coord_y)**2)**0.5)*70000
+            if dist > 600:
+                dist = round(dist)
+                prostrel += 1
+                prostrel_dist += dist
 
-            time_mess1 = mess['t']
-            time_mess = datetime.datetime.utcfromtimestamp(time_mess1+25200).strftime('%Y-%m-%dT%H:%M:%SZ')
+                time_mess1 = mess['t']
+                time_mess = datetime.datetime.utcfromtimestamp(time_mess1+25200).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-            time_mess = time_mess.replace('Z', ' ')
-            time_mess = time_mess.replace('T', ' ')
+                time_mess = time_mess.replace('Z', ' ')
+                time_mess = time_mess.replace('T', ' ')
 
-            prostrel_time.append(str(time_mess)[:20:])
-            prostrel_time.append(time_mess1)
+                prostrel_time.append(str(time_mess)[:20:])
 
-        first_mess_coord_x = mess['pos']['x']
-        first_mess_coord_y = mess['pos']['y']
+                prostrel_time.append(dist)
+
+            first_mess_coord_x = mess['pos']['x']
+            first_mess_coord_y = mess['pos']['y']
 
 print(prostrel)
 print(prostrel_time)
