96c19f7736126c342914ce82ab5cf5c44eb1dde8
Михаил Переводов <perevodovmv@yandex.ru>
2020-06-21 12:02:49 +0700

    Поменял данные вывовда

--- down_data.py
+++ down_data.py
@@ -22,19 +22,19 @@ def api_wialon_dwnData(wialon):
     return res_id
 
 
-def api_wialon_dwnObj(wialon):
-    spec = {
-        'itemsType': 'avl_unit',
-        'propName': 'sys_name',
-        'propValueMask': '*',
-        'sortType': 'sys_name'
-    }
-    interval = {"from": 0, "to": 0}
-    custom_flag = flags.ITEM_DATAFLAG_BASE
-    data = wialon.core_search_items(spec=spec, force=1, flags=custom_flag, **interval)
+# def api_wialon_dwnObj(wialon):
+#     spec = {
+#         'itemsType': 'avl_unit',
+#         'propName': 'sys_name',
+#         'propValueMask': '*',
+#         'sortType': 'sys_name'
+#     }
+#     interval = {"from": 0, "to": 0}
+#     custom_flag = flags.ITEM_DATAFLAG_BASE
+#     data = wialon.core_search_items(spec=spec, force=1, flags=custom_flag, **interval)
     # pprint(data)
-    for key in data['items']:
-        print(f'{key["nm"]} : {key["id"]}')
+    # for key in data['items']:
+    #     print(f'{key["nm"]} : {key["id"]}')
 
 # token = 'db1cee3b1f964df20f8d163a1423b6c6286A919144720D152383E5DD77C6113AD31CDC9A'
 # wialon = Wialon()
