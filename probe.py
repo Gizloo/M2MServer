bc04233a56cfb9be5e3d775bef2ef04eeeb31ff5
perevodov.mv <perevodovmv@yandex.ru>
2020-07-03 16:55:46 +0700

    Добавил вывод для тестирования (1ый)

--- probe.py
+++ probe.py
@@ -1,9 +1,7 @@
-import datetime
-from pprint import pprint
+from wialon import flags, Wialon, WialonError
 
-from wialon import Wialon, WialonError, flags
 
-token = '5978d12e7eb520190d8c97cbc4ac2fbc6708856600D7CB37E18093DD3C6B787DB7D551BA'
+token = 'db1cee3b1f964df20f8d163a1423b6c6286A919144720D152383E5DD77C6113AD31CDC9A'
 wialon = Wialon()
 login = None
 try:
@@ -11,75 +9,5 @@ try:
 except WialonError as e:
     print('Error while login')
 wialon.sid = login['eid']
-
-
-def api_wialon_dwnData(wialon):
-    spec = {
-        'itemsType': 'avl_unit',
-        'propName': 'sys_name',
-        'propValueMask': '*',
-        'sortType': 'sys_name'
-    }
-
-    interval = {"from": 0, "to": 0}
-    custom_flag = flags.ITEM_DATAFLAG_BASE
-    data = wialon.core_search_items(spec=spec, force=1, flags=custom_flag, **interval)
-    return data
-
-
-data = api_wialon_dwnData(wialon)
-# pprint(data)
-timeFrom = 1590944400
-timeTo = 1592326740
-
-unit1 = wialon.messages_load_interval({
-
-    "itemId": 17323425,
-    "timeFrom": timeFrom,
-    "timeTo": timeTo,
-    "flags": 0x0000,
-    "flagsMask": 0xFF00,
-    "loadCount": 0xffffffff})
-
-mess_block = unit1['messages']
-first_mess_coord_x = None
-first_mess_coord_y = None
-
-for mess in mess_block:
-    if mess['pos'] is not None:
-        if mess['pos']['x']:
-            first_mess_coord_x = mess['pos']['x']
-            first_mess_coord_y = mess['pos']['y']
-            break
-
-prostrel = 0
-prostrel_dist = 0
-prostrel_time = []
-print(first_mess_coord_x)
-print(first_mess_coord_y)
-
-
-for mess in mess_block:
-    if mess['pos'] is not None:
-        if mess['pos']['x'] is not None:
-            dist = ((((mess['pos']['x']) - first_mess_coord_x)**2+((mess['pos']['y']) - first_mess_coord_y)**2)**0.5)*70000
-            if dist > 600:
-                dist = round(dist)
-                prostrel += 1
-                prostrel_dist += dist
-
-                time_mess1 = mess['t']
-                time_mess = datetime.datetime.utcfromtimestamp(time_mess1+25200).strftime('%Y-%m-%dT%H:%M:%SZ')
-
-                time_mess = time_mess.replace('Z', ' ')
-                time_mess = time_mess.replace('T', ' ')
-
-                prostrel_time.append(str(time_mess)[:20:])
-
-                prostrel_time.append(dist)
-
-            first_mess_coord_x = mess['pos']['x']
-            first_mess_coord_y = mess['pos']['y']
-
-print(prostrel)
-print(prostrel_time)
+res_id = wialon.core_search_item({"id": 21154797, "flags": 0x00000001})
+print(res_id)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 import datetime
 import time
 from wialon import Wialon, WialonError
-from flask import Flask
+from flask import Flask, render_template
 from Exec_report import execute_report, execute_report2
 from down_data import api_wialon_dwnData
 from handler import handler1, handler2
@@ -18,9 +18,9 @@ def index(requestbot):
     y, m, d, h, min, s = TimeFrom.split('-')
     y1, m1, d1, h1, min1, s1 = TimeTo.split('-')
     t1 = datetime.datetime(int(y), int(m), int(d), int(h), int(min), int(s))
-    from_time = int(str(time.mktime(t1.timetuple()))[:-2]) - 25200
+    from_time = int(str(time.mktime(t1.timetuple()))[:-2])
     t2 = datetime.datetime(int(y1), int(m1), int(d1), int(h1), int(min1), int(s1))
-    to_time = int(str(time.mktime(t2.timetuple()))[:-2]) - 25200
+    to_time = int(str(time.mktime(t2.timetuple()))[:-2])
 
     wialon = Wialon()
     login = None
@@ -45,8 +45,6 @@ def index(requestbot):
     callback_retr += str(calb1[3][1])[:calb1[4][1].find(" "):] + ';'
     callback_retr += str(calb1[5][1])[:calb1[5][1].find(" "):] + ';'
     callback_retr += str(calb1[6][1])[:calb1[6][1].find(" "):] + ';'
-
-
     callback = handler1(calb2, calb3, milleage, pr_count, pr_dist)
 
     callback_retr += str(callback.data_status) + ';' + str(callback.track_status) \
@@ -70,9 +68,142 @@ def norm(requesthandler):
     y, m, d, h, min, s = TimeFrom.split('-')
     y1, m1, d1, h1, min1, s1 = TimeTo.split('-')
     t1 = datetime.datetime(int(y), int(m), int(d), int(h), int(min), int(s))
-    from_time = int(str(time.mktime(t1.timetuple()))[:-2]) - 25200
+    from_time = int(str(time.mktime(t1.timetuple()))[:-2])
     t2 = datetime.datetime(int(y1), int(m1), int(d1), int(h1), int(min1), int(s1))
-    to_time = int(str(time.mktime(t2.timetuple()))[:-2]) - 25200
+    to_time = int(str(time.mktime(t2.timetuple()))[:-2])
+
+
+    wialon = Wialon()
+    login = None
+    try:
+        login = wialon.token_login(token=str(token))
+    except WialonError as e:
+        print('Error while login')
+    wialon.sid = login['eid']
+    res_id = api_wialon_dwnData(wialon)
+
+    if res_id:
+        volume_tank, end_fuel_f, fuel_up_f, fuel_down, start_fuel_f = execute_report2(res_id, wialon, ID, from_time, to_time)
+    else:
+        return 'No API resourses'
+
+    consum_f = round(start_fuel_n + fuel_up - end_fuel_f, 2)
+
+    callback = handler2(volume_tank, consum_f, fuel_up_f, fuel_down, fuel_up, consumption_n, start_fuel_n, start_fuel_f)
+    callback_retr = ''
+
+    if callback.fuel_up:
+        callback_retr += "Заправка не сходится:" + str(round(fuel_up_f - fuel_up, 2)) + ";"
+    elif callback.nedoliv:
+        callback_retr += "Недолив:" + str(round((fuel_up - fuel_up_f), 2)) + ";"
+    else:
+        callback_retr += "Ok;"  # Заправка ОК
+
+    if callback.fuel_down:
+        callback_retr += str(round(fuel_down, 2)) + ";"
+    else:
+        callback_retr += "0;"  # Слив ОК
+
+    if callback.fuel_start:
+        callback_retr += "Нач ур. не сходится!;"
+    else:
+        callback_retr += "Ok;"  # Нач ур. ОК
+
+    """Дальше один из 4 вариантов"""
+    if callback.short:
+        callback_retr += "Короткая поездка, списание по норме;"
+
+    elif callback.perejog:
+        callback_retr += "Пережог топлива:" + str(round(consum_f - consumption_n, 2)) + ", списание по факту;"
+
+    elif callback.economy and not callback.nedoliv and not callback.fuel_down:
+        callback_retr += "Экономия топлива:" + str(round(consumption_n - consum_f, 2)) + ", списание по факту;"
+
+    else:
+        callback_retr += "Расход сходится"
+
+    return callback_retr
+
+
+@app.route("/KrayDEO/test/<requestbot>", methods=['GET'])
+def test(requestbot):
+    req_b = str(requestbot)
+    token, ID, TimeFrom, TimeTo = req_b.split(';')
+    y, m, d, h, min, s = TimeFrom.split('-')
+    y1, m1, d1, h1, min1, s1 = TimeTo.split('-')
+    t1 = datetime.datetime(int(y), int(m), int(d), int(h), int(min), int(s))
+    from_time = int(str(time.mktime(t1.timetuple()))[:-2])
+    t2 = datetime.datetime(int(y1), int(m1), int(d1), int(h1), int(min1), int(s1))
+    to_time = int(str(time.mktime(t2.timetuple()))[:-2])
+
+    wialon = Wialon()
+    login = None
+    try:
+        login = wialon.token_login(token=str(token))
+    except WialonError as e:
+        print('Error while login')
+    wialon.sid = login['eid']
+    res_id = api_wialon_dwnData(wialon)
+
+    if res_id:
+        calb1, calb2, calb3, pr_count, pr_dist = execute_report(res_id, wialon, ID, from_time, to_time)
+    else:
+        return 'No API resourses'
+
+    milleage = int(str(calb1[1][1])[:calb1[1][1].find("."):])
+
+    callback_retr = ''
+    callback_retr += str(calb1[1][1])[:calb1[1][1].find(" "):] + ';'
+    probeg = calb1[1][1].replace("km", 'км.')
+    callback_retr += str(calb1[2][1]) + ';'
+    motoh = calb1[2][1]
+    callback_retr += str(calb1[4][1])[:calb1[4][1].find(" "):] + ';'
+    start_fuel = calb1[4][1].replace("l", 'л.')
+
+    callback_retr += str(calb1[3][1])[:calb1[3][1].find(" "):] + ';'
+    end_fuel = calb1[3][1].replace("l", 'л.')
+
+    callback_retr += str(calb1[5][1])[:calb1[5][1].find(" "):] + ';'
+    fuel_up = calb1[5][1].replace("l", 'л.')
+    callback_retr += str(calb1[6][1])[:calb1[6][1].find(" "):] + ';'
+    fuel_down = calb1[6][1].replace("l", 'л.')
+    callback = handler1(calb2, calb3, milleage, pr_count, pr_dist)
+
+    callback_retr += str(callback.data_status) + ';' + str(callback.track_status) \
+                     + ';' + str(callback.dut_status) + ';' + str(callback.ign_status) + ';'
+
+    callback_retr += '\n'
+    data_obj = wialon.core_search_item({"id": ID, "flags": 0x00000001})
+    name_obj = data_obj['item']['nm']
+    callback_retr += f'{"Детализация отчета":*^30}'
+    callback_retr += f'Объект{name_obj}'
+    start_period = f'{d}.{m}.{y}. {h}:{min}:{s}'
+    end_period = f'{d1}.{m1}.{y1}. {h1}:{min1}:{s1}'
+    return render_template('test_first.html', name=name_obj, probeg=probeg, motoh=motoh, start_fuel=start_fuel,
+                           end_fuel=end_fuel, fuel_up=fuel_up, fuel_down=fuel_down, start_period=start_period,
+                           end_period=end_period, data_status=callback.data_status, track_status=callback.track_status,
+                           dut_status=callback.dut_status, ign_status=callback.ign_status)
+
+
+
+@app.route("/KrayDEO/test_norm/<requesthandler>", methods=['GET'])
+def test_norm(requesthandler):
+
+    req_b = str(requesthandler)
+    token, ID, TimeFrom, TimeTo, start_fuel_n, consumption_n, fuel_up = req_b.split(';')
+
+    start_fuel_n.replace(',', '.')
+    consumption_n.replace(',', '.')
+    fuel_up.replace(',', '.')
+
+    start_fuel_n, consumption_n, fuel_up = float(start_fuel_n), float(consumption_n), float(fuel_up)
+
+    y, m, d, h, min, s = TimeFrom.split('-')
+    y1, m1, d1, h1, min1, s1 = TimeTo.split('-')
+    t1 = datetime.datetime(int(y), int(m), int(d), int(h), int(min), int(s))
+    from_time = int(str(time.mktime(t1.timetuple()))[:-2])
+    t2 = datetime.datetime(int(y1), int(m1), int(d1), int(h1), int(min1), int(s1))
+    to_time = int(str(time.mktime(t2.timetuple()))[:-2])
 
 
     wialon = Wialon()
@@ -128,4 +259,4 @@ def norm(requesthandler):
 
 
 if __name__ == "__main__":
-    app.run(host='10.128.0.2', port=4567, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=4567, debug=True, threaded=True)
