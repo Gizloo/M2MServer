2f905eb446b8922e5d66dcfd4bb5f232a8ef55f8
Михаил Переводов <perevodovmv@yandex.ru>
2020-07-05 18:41:37 +0700

    Добавил тест для норм

--- down_data.py
+++ down_data.py
@@ -18,8 +18,8 @@ def api_wialon_dwnData(wialon):
     res_id = None
     for count in data['items']:
         if 'api' in str(count['nm']):
-        	res_id = count['id']
-   
+            res_id = count['id']
+
     return res_id
 
 
@@ -33,9 +33,9 @@ def api_wialon_dwnData(wialon):
 #     interval = {"from": 0, "to": 0}
 #     custom_flag = flags.ITEM_DATAFLAG_BASE
 #     data = wialon.core_search_items(spec=spec, force=1, flags=custom_flag, **interval)
-    # pprint(data)
-    # for key in data['items']:
-    #     print(f'{key["nm"]} : {key["id"]}')
+# pprint(data)
+# for key in data['items']:
+#     print(f'{key["nm"]} : {key["id"]}')
 
 # token = 'db1cee3b1f964df20f8d163a1423b6c6286A919144720D152383E5DD77C6113AD31CDC9A'
 # wialon = Wialon()
@@ -45,5 +45,15 @@ def api_wialon_dwnData(wialon):
 # except WialonError as e:
 #     print('Error while login')
 # wialon.sid = login['eid']
-# res_id = api_wialon_dwnData(wialon)
-# print(res_id)
+# # res_id = api_wialon_dwnData(wialon)
+# # print(res_id)
+# ID = 21113804
+# object_ = wialon.core_search_item ({"id": ID,
+#                                       "flags": 0x00001000})
+#
+# # pprint(object_)
+# sens = object_['item']['sens']
+# for num, sen in sens.items():
+#     # print(sen)
+#     print(f'№{num} : Имя датчика {sen["n"]} Тип датчика {sen["t"]}')
+#     print(type(sen['t']))
--- server.py
+++ server.py
@@ -32,7 +32,7 @@ def index(requestbot):
     res_id = api_wialon_dwnData(wialon)
 
     if res_id:
-        calb1, calb2, calb3, pr_count, pr_dist = execute_report(res_id, wialon, ID, from_time, to_time)
+        calb1, pr_count, pr_dist, units4, units5 = execute_report(res_id, wialon, ID, from_time, to_time)
     else:
         return 'No API resourses'
 
@@ -41,11 +41,11 @@ def index(requestbot):
     callback_retr = ''
     callback_retr += str(calb1[1][1])[:calb1[1][1].find(" "):] + ';'
     callback_retr += str(calb1[2][1]) + ';'
-    callback_retr += str(calb1[4][1])[:calb1[3][1].find(" "):] + ';'
-    callback_retr += str(calb1[3][1])[:calb1[4][1].find(" "):] + ';'
+    callback_retr += str(calb1[4][1])[:calb1[4][1].find(" "):] + ';'
+    callback_retr += str(calb1[3][1])[:calb1[3][1].find(" "):] + ';'
     callback_retr += str(calb1[5][1])[:calb1[5][1].find(" "):] + ';'
     callback_retr += str(calb1[6][1])[:calb1[6][1].find(" "):] + ';'
-    callback = handler1(calb2, calb3, milleage, pr_count, pr_dist)
+    callback = handler1(calb1, milleage, pr_count, pr_dist, from_time, to_time, wialon, units4, units5, res_id, ID)
 
     callback_retr += str(callback.data_status) + ';' + str(callback.track_status) \
                      + ';' + str(callback.dut_status) + ';' + str(callback.ign_status) + ';'
@@ -71,7 +71,6 @@ def norm(requesthandler):
     from_time = int(str(time.mktime(t1.timetuple()))[:-2])
     t2 = datetime.datetime(int(y1), int(m1), int(d1), int(h1), int(min1), int(s1))
     to_time = int(str(time.mktime(t2.timetuple()))[:-2])
-
 
     wialon = Wialon()
     login = None
@@ -146,7 +145,7 @@ def test(requestbot):
     res_id = api_wialon_dwnData(wialon)
 
     if res_id:
-        calb1, calb2, calb3, pr_count, pr_dist = execute_report(res_id, wialon, ID, from_time, to_time)
+        calb1, pr_count, pr_dist, units4, units5 = execute_report(res_id, wialon, ID, from_time, to_time)
     else:
         return 'No API resourses'
 
@@ -167,23 +166,20 @@ def test(requestbot):
     fuel_up = calb1[5][1].replace("l", 'л.')
     callback_retr += str(calb1[6][1])[:calb1[6][1].find(" "):] + ';'
     fuel_down = calb1[6][1].replace("l", 'л.')
-    callback = handler1(calb2, calb3, milleage, pr_count, pr_dist)
+    callback = handler1(calb1, milleage, pr_count, pr_dist, from_time, to_time, wialon, units4, units5, res_id, ID)
 
     callback_retr += str(callback.data_status) + ';' + str(callback.track_status) \
                      + ';' + str(callback.dut_status) + ';' + str(callback.ign_status) + ';'
 
-    callback_retr += '\n'
     data_obj = wialon.core_search_item({"id": ID, "flags": 0x00000001})
     name_obj = data_obj['item']['nm']
-    callback_retr += f'{"Детализация отчета":*^30}'
-    callback_retr += f'Объект{name_obj}'
     start_period = f'{d}.{m}.{y}. {h}:{min}:{s}'
     end_period = f'{d1}.{m1}.{y1}. {h1}:{min1}:{s1}'
     return render_template('test_first.html', name=name_obj, probeg=probeg, motoh=motoh, start_fuel=start_fuel,
                            end_fuel=end_fuel, fuel_up=fuel_up, fuel_down=fuel_down, start_period=start_period,
-                           end_period=end_period, data_status=callback.data_status, track_status=callback.track_status,
-                           dut_status=callback.dut_status, ign_status=callback.ign_status)
-
+                           end_period=end_period, data_status=callback.data_status, data_info=callback.data_info,
+                           track_status=callback.track_status, track_info=callback.track_info,
+                           dut_status=callback.dut_info, ign_status=callback.ign_info, callback=callback_retr)
 
 
 @app.route("/KrayDEO/test_norm/<requesthandler>", methods=['GET'])
@@ -205,7 +201,6 @@ def test_norm(requesthandler):
     t2 = datetime.datetime(int(y1), int(m1), int(d1), int(h1), int(min1), int(s1))
     to_time = int(str(time.mktime(t2.timetuple()))[:-2])
 
-
     wialon = Wialon()
     login = None
     try:
@@ -226,37 +221,64 @@ def test_norm(requesthandler):
     callback_retr = ''
 
     if callback.fuel_up:
+
         callback_retr += "Заправка не сходится:" + str(round(fuel_up_f - fuel_up, 2)) + ";"
+        callback_fuel_info = f"Заправка не сходится, разница = {round(fuel_up_f - fuel_up, 2)}"
+
     elif callback.nedoliv:
         callback_retr += "Недолив:" + str(round((fuel_up - fuel_up_f), 2)) + ";"
+        callback_fuel_info = f"Недолив, объемом = {round((fuel_up - fuel_up_f), 2)}"
     else:
         callback_retr += "Ok;"  # Заправка ОК
+        callback_fuel_info = f"Заправка ОК"
 
     if callback.fuel_down:
         callback_retr += str(round(fuel_down, 2)) + ";"
     else:
         callback_retr += "0;"  # Слив ОК
-
+    diff_start = round(float(start_fuel_n)-float(start_fuel_f))
     if callback.fuel_start:
         callback_retr += "Нач ур. не сходится!;"
+        callback_start_info = f"Нач ур. не сходится! Разница в {diff_start} л."
     else:
         callback_retr += "Ok;"  # Нач ур. ОК
+        callback_start_info = "Нач. уровень сходится"
 
     """Дальше один из 4 вариантов"""
     if callback.short:
         callback_retr += "Короткая поездка, списание по норме;"
+        callback_consum_info = "Короткая поездка, списание по норме, потрачено меньше 2% от бака"
 
     elif callback.perejog:
         callback_retr += "Пережог топлива:" + str(round(consum_f - consumption_n, 2)) + ", списание по факту;"
+        callback_consum_info = f"Пережог топлива, объем = {round(consum_f - consumption_n, 2)}, списание по факту"
 
     elif callback.economy and not callback.nedoliv and not callback.fuel_down:
         callback_retr += "Экономия топлива:" + str(round(consumption_n - consum_f, 2)) + ", списание по факту;"
+        callback_consum_info = f"Экономия топлива, объем = {round(consumption_n - consum_f, 2)}, списание по факту"
 
     else:
         callback_retr += "Расход сходится"
+        callback_consum_info = 'Расход сходится (В пределах погрешности)'
 
-    return callback_retr
+    if float(fuel_down) > 0:
+        fuel_down_info = f'Слито {fuel_down}! Необходимо провести служебное расследование'
+    else:
+        fuel_down_info ='Cливов не зафиксировано'
+
+    end_fuel_n = float(start_fuel_n) - float(consumption_n) + float(fuel_up)
+    data_obj = wialon.core_search_item({"id": ID, "flags": 0x00000001})
+    name_obj = data_obj['item']['nm']
+    start_period = f'{d}.{m}.{y}. {h}:{min}:{s}'
+    end_period = f'{d1}.{m1}.{y1}. {h1}:{min1}:{s1}'
+    return render_template('test_norm.html', name=name_obj, start_p=start_period,
+                           end_period=end_period, start_fuel_n=start_fuel_n,
+                           start_fuel_f=start_fuel_f, consumption_n=consumption_n, consum_f=consum_f, fuel_up=fuel_up,
+                           fuel_up_f=fuel_up_f, fuel_down=fuel_down, callback=callback_retr,
+                           callback_fuel_info=callback_fuel_info, callback_consum_info=callback_consum_info,
+                           volume_tank=volume_tank, end_fuel_n=end_fuel_n, end_fuel_f=end_fuel_f,
+                           callback_start_info=callback_start_info, fuel_down_info=fuel_down_info)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4567, debug=True, threaded=True)
+    app.run(host='10.128.0.2', port=4567, debug=True, threaded=True)
